95bd09ac30ea94c22fa22c90536aca8a39438958
saykatb <saykatbhowmik2@gmail.com>
2022-04-10 18:21:36 +0600

    first commit

---
+++ src/Components/Cart/Cart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Cart = ({cart}) => {
+    return (
+        <div>
+            <h4>order summary</h4>
+            <p>Selected items:{cart.length}</p>
+        </div>
+    );
+};
+
+export default Cart;
